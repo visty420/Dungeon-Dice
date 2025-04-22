a40d7848d8c512df3879b70497ad6b1095243ceb
visty420 <maneaalex6851@gmail.com>
2025-04-22 11:37:13 +0300

    Added critical hits and misses to the game

--- dungeons-game/game/combat.go
+++ dungeons-game/game/combat.go
@@ -7,11 +7,35 @@ import (
 
 func CombatRound(player *Character, monster *Monster) {
 	fmt.Println("\n---Combat Turn---")
-	playerRoll := rand.Intn(player.Strength) + 1
-	monsterRoll := rand.Intn(monster.Strength) + 1
+	playerRoll := rand.Intn(20) + 1
+	monsterRoll := rand.Intn(20) + 1
 
 	fmt.Printf("You roll: %d \n", playerRoll)
 	fmt.Printf("The monster rolls: %d\n", monsterRoll)
+
+	if playerRoll == 1 {
+		selfDamage := rand.Intn(5) + 1
+		player.HP -= selfDamage
+		fmt.Printf("CRITICAL MISS! You stumble and hurt yourself for %d damage! Your HP: %d\n", selfDamage, player.HP)
+	}
+
+	if playerRoll == 20 {
+		criticalDamage := player.Strength * 2
+		monster.HP -= criticalDamage
+		fmt.Printf("CRITICAL HIT! You unleash the powers of heavon upon the damned beast, dealing %d damage! Monster's HP: %d\n", criticalDamage, monster.HP)
+	}
+
+	if monsterRoll == 1 {
+		selfDamage := rand.Intn(5) + 1
+		monster.HP -= selfDamage
+		fmt.Printf("THE FOOLISH CREATURE MISSES YOU! The monster stumbles and hits itself for %d damage like a moron! Monster's HP: %d\n", selfDamage, monster.HP)
+	}
+
+	if monsterRoll == 20 {
+		criticalDamage := monster.Strength * 2
+		player.HP -= criticalDamage
+		fmt.Printf("OUCH! The monster curses upon your bloodline and hits you like a truck for %d damage! Your HP: %d\n", criticalDamage, player.HP)
+	}
 
 	if playerRoll > monsterRoll {
 		damage := playerRoll - monsterRoll
